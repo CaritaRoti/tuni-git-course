e64d253c85d3580f21a5c341a78541d24e8661b5
Carita <carita.roti@gmail.com>
2026-05-30 04:37:15 +0300

    ylikirjoitetaan mergesort commitin  dd8c56ce mukaan

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,4 +1,3 @@
-
 def debug_print(debug_msg=None, **kwargs):
 
     if debug_msg:
@@ -44,7 +43,10 @@ def merge(left, right):
 
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
+
     input_list = input_str.split(",")
+    debug_print(input_list=input_list)
+
     value_list = []
     for x in input_list:
         try:
